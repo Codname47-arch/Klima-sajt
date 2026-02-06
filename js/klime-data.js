06f672671aa5678c468fd776e4c05b5c12393428
Codname47-arch <stefansimic226@gmail.com>
2026-02-06 16:34:06 +0100

    Update klime-data.js

--- js/klime-data.js
+++ js/klime-data.js
@@ -3,24 +3,32 @@ const KLIME = [
     naziv: "Tesla Inverter 12",
     opis: "Grijanje do -15°C, tih i stabilan rad",
     cijena: "750 KM sa ugradnjom",
+    stara cijena:  "850 KM",
+    popust:"  10%",
     slika: "img/tesla12.jpg"
   },
   {
     naziv: "Tesla Inverter 9",
     opis: "Idealna za manje prostorije",
     cijena: "690 KM sa ugradnjom",
+    stara cijena:  "850 KM",
+    popust:"  10%",
     slika: "img/tesla9.jpg"
   },
   {
     naziv: "Tesla Inverter 18",
     opis: "Veća snaga za veće prostore",
     cijena: "1.050 KM sa ugradnjom",
+    stara cijena:  "1200 KM",
+    popust:"  10%",
     slika: "img/tesla18.jpg"
   },
   {
     naziv: "Tesla Inverter 24",
     opis: "Za poslovne i velike prostore",
     cijena: "1.350 KM sa ugradnjom",
+    stara cijena:  "1450 KM",
+    popust:"  10%",
     slika: "img/tesla24.jpg"
   }
 ];
