c775a632bef1c836aada745507293e560ef19046
Codname47-arch <stefansimic226@gmail.com>
2026-02-06 18:23:05 +0100

    Update klime-data.js

--- js/klime-data.js
+++ js/klime-data.js
@@ -1,34 +1,58 @@
+// data/klime.js
 window.KLIME = [
   {
+    id: "tesla-12",
     naziv: "Tesla Inverter 12",
-    opis: "Grijanje do -15°C, tih i stabilan rad",
-    cijena: "750 KM sa ugradnjom",
-    staraCijena: "820 KM",
-    popust: "-9%",
+    brand: "Tesla",
+    btu: "12k",
+    namjena: ["hladjenje", "dogrijavanje", "grijanje"],
+    cijenaSaUgradnjom: 750,
+    popularnost: 95,
+    featured: true,
     slika: "img/tesla12.jpg"
   },
   {
+    id: "tesla-9",
     naziv: "Tesla Inverter 9",
-    opis: "Idealna za manje prostorije",
-    cijena: "690 KM sa ugradnjom",
-    staraCijena: "760 KM",
-    popust: "-9%",
+    brand: "Tesla",
+    btu: "9k",
+    namjena: ["hladjenje", "dogrijavanje"],
+    cijenaSaUgradnjom: 690,
+    popularnost: 82,
+    featured: true,
     slika: "img/tesla9.jpg"
   },
   {
-    naziv: "Tesla Inverter 18",
-    opis: "Veća snaga za veće prostore",
-    cijena: "1.050 KM sa ugradnjom",
-    staraCijena: "1.150 KM",
-    popust: "-9%",
-    slika: "img/tesla18.jpg"
+    id: "haier-12",
+    naziv: "Haier Inverter 12",
+    brand: "Haier",
+    btu: "12k",
+    namjena: ["hladjenje", "grijanje"],
+    cijenaSaUgradnjom: 890,
+    popularnost: 88,
+    featured: false,
+    slika: "img/haier12.jpg"
   },
   {
+    id: "vivax-18",
+    naziv: "Vivax Inverter 18",
+    brand: "Vivax",
+    btu: "18k",
+    namjena: ["hladjenje", "grijanje"],
+    cijenaSaUgradnjom: 1150,
+    popularnost: 91,
+    featured: true,
+    slika: "img/vivax18.jpg"
+  },
+  {
+    id: "tesla-24",
     naziv: "Tesla Inverter 24",
-    opis: "Za poslovne i velike prostore",
-    cijena: "1.350 KM sa ugradnjom",
-    staraCijena: "1.480 KM",
-    popust: "-9%",
+    brand: "Tesla",
+    btu: "24k",
+    namjena: ["hladjenje", "grijanje"],
+    cijenaSaUgradnjom: 1350,
+    popularnost: 79,
+    featured: false,
     slika: "img/tesla24.jpg"
   }
 ];
